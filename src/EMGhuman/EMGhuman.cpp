afa260ec50970af00fa7b262ea63f00ba2a4eb82
Serena Ivaldi <serena.ivaldi@inria.fr>
2017-08-03 19:06:46 +0200

    adding thread to EMGhuman, plus more variables and rpc procedure

--- src/EMGhuman/EMGhuman.cpp
+++ src/EMGhuman/EMGhuman.cpp
@@ -22,6 +22,7 @@
 #include <fstream>
 #include <iostream>
 #include <stdio.h>
+#include <deque>
 
 using namespace std;
 using namespace yarp::os;
@@ -33,6 +34,10 @@ using namespace yarp::sig;
 #define DSCPAs(S,V) cout<<"  "<< S <<" : "<<V.toString()<<endl;
 #define DSCPAd(S,V) cout<<"  "<< S <<" : "<<V<<endl;
 
+#define STATUS_STOPPED      0
+#define STATUS_STREAMING    1
+#define STATUS_CALIBRATION  2
+
 
 //===============================
 //        EMGhuman THREAD
@@ -41,17 +46,40 @@ using namespace yarp::sig;
 class EMGhumanThread: public RateThread
 {
     protected:
-
+        // status information
+        int status, prevStatus;
         // name used for the ports
         string name;
         // current time
         double curTime;
+        // stiffness
+        Vector stiffness;
+        // icc
+        Vector icc;
+        // effort
+        Vector effort;
+        // max EMG value
+        Vector emg_max, calibration_emg_max;
+        // min EMG value
+        Vector emg_min, calibration_emg_min;
+        // mean EMG value
+        Vector emg_mean, calibration_emg_mean;
+        // flag is Calibrated 0=no 1=with default values 2=after calibration procedure
+        int isCalibrated;
+
+        //input ports
+        Bottle inEmg;
+        BufferedPort<Bottle> inPortEmg;
+        //output ports
+        Bottle outEmg, outIcc, outStiffness, outEffort;
+        BufferedPort<Bottle> outPortEmg, outPortIcc, outPortStiffness, outPortEffort;
 
     public: 
 
     EMGhumanThread(const double _period, string _name): RateThread(int(_period*1000.0))
     {
         name = _name;
+        status = STATUS_STOPPED;
         yInfo("EMGhuman: thread created");
 
     }
@@ -81,18 +109,86 @@ class EMGhumanThread: public RateThread
         // cyclic operations should be put here!
         curTime = Time::now();
 
-        // read EMG values from EMG server
+        if(status==STATUS_STOPPED)
+        {
+            // probably do nothing
 
-        // compute stiffness
+        }
+        else
+        {
+            
+            // read EMG values from EMG server
+
+            // compute stiffness
         
-        // compute ICC
+            // compute ICC
 
-        // compute effort 
+            // compute effort 
 
-        // send output to ports
+            // send output to ports
+
+
+            if(status==STATUS_CALIBRATION)
+            {
+            // do the necessary things for the calibration
+
+            }
 
 
 
+        }
+
+    }
+    //end run
+
+    void startStreaming()
+    {
+        if(isCalibrated<1) yWarning("EMGhuman: streaming but not calibrated yet!");
+        status=STATUS_STREAMING;
+    }
+
+    void stopStreaming()
+    {
+        status=STATUS_STOPPED;
+    }
+
+    void startCalibration()
+    {
+        // save current state as we want to go back to this state after the calibration
+        prevStatus=status;
+        status=STATUS_CALIBRATION;
+    }
+
+    void stopCalibration()
+    {
+        // go back to the status that was before the calibration (stopped or streaming)
+        status=prevStatus;
+    }
+
+    int getStatus()
+    {
+        return status;
+    }
+
+    bool setCalibrationValues(Vector _emg_calib_max, Vector _emg_calib_min)
+    {
+        if((calibration_emg_max.size()!=_emg_calib_max.size())||(calibration_emg_min.size()!=_emg_calib_min.size()))
+        {
+            yWarning("EMGhuman: different size for the calibration parameters that are manually provided. Ignoring calibration.");
+            return false;
+        }
+        calibration_emg_max = _emg_calib_max;
+        calibration_emg_min = _emg_calib_min;
+
+        return true;
+    }
+
+    bool computeCalibration()
+    {
+        //TODO
+        //procedure for calibrating the EMG with the MAX V C
+
+        return true;
     }
 
 
@@ -115,6 +211,24 @@ private:
     string name;
     // rate of the human thread, expressed in seconds: e.g, 20 ms => 0.02
     double rate;
+    // calibration 
+    bool calibration;
+    // calibration duration
+    double calibration_duration;
+    // type of calibration 2=arm2 / 4=arm4 / 8=arm8
+    int calibration_type;
+    // calibration default values
+    Vector calibration_emg_max, calibration_emg_min;
+    // use filtered data 0=no, 1=rmse, 2=rmse+filtered
+    int use_filtered_data;
+    // auto-connect to the ports (VERY RISKY)
+    bool autoconnect;
+    // numbers of sensors that we will use here
+    int numberOfSensors;
+    // sensors ids
+    Vector sensorIds;
+    // sensor names
+    deque<string> sensorNames;
 
     // human thread
     EMGhumanThread *humanThread;
@@ -127,6 +241,11 @@ public:
         count=0;
         rate=0.01;
         name="EMGhuman";
+        calibration_emg_max.resize(8,0.0); 
+        calibration_emg_min.resize(8,0.0);
+        sensorIds.resize(8);
+        calibration=false;
+        calibration_duration=10.0;
     }
 
     //---------------------------------------------------------
@@ -154,39 +273,62 @@ public:
         if (cmd=="list" || cmd=="help")
         {
             reply.clear();
-            reply.addString("Here is the list of available commands: ");
+            reply.addString("Here is the list of available commands:");
+            reply.addString("stop");
+            reply.addString("start");
+            reply.addString("status");
+            reply.addString("calibration_from_file");
+            reply.addString("calibration_online");
             return true;
         }  
         else if(cmd=="stop")
         {
-            
+            humanThread->stopStreaming();
+            reply.clear(); reply.addString("OK");
+            return true;
         }
         else if(cmd=="start")
         {
-
+            humanThread->startStreaming();
+            reply.clear(); reply.addString("OK");
+            return true;
         } 
         else if(cmd=="status")
         {
+            int curStatus=humanThread->getStatus();
+
+            reply.clear();
+            if(curStatus==STATUS_STOPPED) reply.addString(" Status = STOPPED");
+            else if(curStatus==STATUS_CALIBRATION) reply.addString(" Status = CALIBRATION");
+            else if(curStatus==STATUS_STREAMING) reply.addString(" Status = STREAMING");
+            else reply.addString(" Status = IMPOSSIBLE");
+
+            return true;
 
         }
-        else if(cmd=="filtering")
+        else if(cmd=="calibration_from_file")
         {
-            ConstString config = command.get(1).asString();
-            cout<<"second command = "<<config<<endl;
-            if(config=="off")
+            bool ret=humanThread->setCalibrationValues(calibration_emg_max,calibration_emg_min);
+            if(ret==false)
             {
-
-            }
-            else if(config=="on")
-            {
-
-
+                reply.clear();
+                reply.addString("Calibration not done - mismatch in size of max/min values");
             }
             else
             {
-
+                humanThread->computeCalibration();  
+                reply.clear();
+                reply.addString("Calibration done");
             }
-
+            return true;
+            
+        }
+        else if(cmd=="calibration_online")
+        {
+            humanThread->startCalibration();
+            reply.clear();
+            reply.addString("OK");
+            return true;
         }
         else
         {
@@ -272,10 +414,12 @@ public:
         }
        
     
-        //attach a port to the module, so we can send messages
-        //and choose the type of grasp to execute
+        //attach a rpc port to the module
         //messages received from the port are redirected to the respond method
-        rpc.open(string("/"+name+"/rpc:i").c_str());
+        // to connect to the module do:
+        // yarp rpc /EMGhuman/rpc 
+        // yarp rpc /human_operator/rpc
+        rpc.open(string("/"+name+"/rpc").c_str());
         attach(rpc);
         yInfo("EMGhuman: RPC port attached");
 
